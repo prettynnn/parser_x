0be75da754b85555b4600d3d61d6260cf5234b1e
nnn <dontlistenthem@protonmail.com>
2025-12-14 20:38:25 +0300

    Update exceptions.py

--- files/exceptions.py
+++ files/exceptions.py
@@ -1,1 +1,20 @@
+class RateLimitError(Exception):
+    pass
 
+class ServerError(Exception):
+    pass
+
+class NoneJsonError(Exception):
+    pass
+
+class IDJsonError(Exception):
+    pass
+
+class TweetJsonError(Exception):
+    pass
+
+class ScanningError(Exception):
+    pass
+
+class FailedConnectDatabase(Exception):
+    pass
